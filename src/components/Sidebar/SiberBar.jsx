1f1f3e55072279b31e5b7bbd1f067f9785ded184
slopez-2018151 <slopez-2018151@kinal.edu.gt>
2023-06-10 21:07:54 -0600

    View and create of the services and products

--- src/components/Sidebar/SiberBar.jsx
+++ src/components/Sidebar/SiberBar.jsx
@@ -30,57 +30,35 @@ export const SiberBar = () => {
                             </a>
                         </li>
                         <li className="has-subnav">
-                            <a href="#" onClick={() => navigate ('/typeAccount')}>
+                            <a href="#" onClick={() => navigate('/typeAccount')}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                    <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+                                </svg>
                                 <span className="nav-text">
                                     View Type Accounts
                                 </span>
                             </a>
                         </li>
                         <li className="has-subnav">
-                            <a href="#">
-                                <i className="fa fa-camera-retro fa-2x"></i>
+                            <a href="#" onClick={() => navigate('/products')}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
+                                    <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z" />
+                                </svg>
                                 <span className="nav-text">
-                                    Survey Photos
+                                    Products View
                                 </span>
                             </a>
                         </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-film fa-2x"></i>
+                        <li className="has-subnav">
+                            <a href="#" onClick={() => navigate('/services')}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-bookmark-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M6 1h6v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8V1z" />
+                                    <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z" />
+                                    <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z" />
+                                </svg>
                                 <span className="nav-text">
-                                    Surveying Tutorials
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-book fa-2x"></i>
-                                <span className="nav-text">
-                                    Surveying Jobs
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-cogs fa-2x"></i>
-                                <span className="nav-text">
-                                    Tools & Resources
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-map-marker fa-2x"></i>
-                                <span className="nav-text">
-                                    Member Map
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-info fa-2x"></i>
-                                <span className="nav-text">
-                                    Documentation
+                                    Services View
                                 </span>
                             </a>
                         </li>
